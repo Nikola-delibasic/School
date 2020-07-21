b6d6ac3f97240f4cdb6bfe745b42ae7dec905819
Nikola <nkl.delibasic@gmail.com>
2020-07-21 18:49:10 +0200

    Class Student created v1.10

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+import student.Student;
+
+public class Main {
+    
+    public static void main(String[] args) {
+     Student Stefan = new Student();
+     Stefan.info();
+     
+   
+    
+     
+       
+    }
+    
+}
